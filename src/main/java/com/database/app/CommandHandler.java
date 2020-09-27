3d61aeaa68f2b9a0ccc04eae6c04503ee7fb55a3
14jthaxton <14jthaxton@gmail.com>
2020-09-26 18:18:08 -0700

    finish write funcitonality

--- src/main/java/com/database/app/CommandHandler.java
+++ src/main/java/com/database/app/CommandHandler.java
@@ -1,6 +1,9 @@
 package com.database.app;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 import java.io.IOException;
 
 public class CommandHandler 
@@ -10,29 +13,58 @@ public class CommandHandler
       System.out.print("DBLite> ");
       BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
       try {
-        String[] splitInput = reader.readLine().toLowerCase().split(" ");
-        switch(splitInput[0]) {
-          case "schema:create":
+        String[] splitInput = reader.readLine().split(" ");
+
+        if(splitInput[0].equals("schema:create")) {
             Schema.create();
-            break;
-          case "schema:drop":
+            
+        } else if (splitInput[0].equals("schema:drop")) {
             Schema.drop();
-            break;
-          case "schema:migrate":
-            Schema.migrate();
-            break;
-          case "schema:migrate:create":
-            Migration.create(splitInput[1], splitInput[2]);
-            break;
-          case "write":
-            System.out.print("DBLite> ");
-            // BufferedReader jsonInput = new BufferedReader(new InputStreamReader(System.in));
-            // Model.write(jsonInput);
-          case "exit":
-            return;
-          default:
-            System.out.println("Invalid input. Try again.");
+            
+        } else if (splitInput[0].equals("schema:migrate")) {
+          Schema.migrate();
+          
+        } else if (splitInput[0].equals("schema:migrate:create")) {
+          Migration.create(splitInput[1], splitInput[2]);
+          
+        } else if (splitInput[0].equals("write")) {
+          try {
+          JSONParser parser = new JSONParser();
+          JSONObject json = (JSONObject) parser.parse(splitInput[2]);
+          System.out.println(splitInput[2]);
+          System.out.println(json);
+          System.out.println(splitInput[1]);
+          Writer.write(splitInput[1].toString(), json);
+          } catch(Exception e) {
+            System.out.println("Unable to write");
+          }
+        } else if (splitInput[0].equals("exit")) {
+          return;
+        } else {
+          System.out.println("Invalid input. Try again.");
         }
+        // switch(splitInput[0]) {
+        //   case "schema:create":
+        //     Schema.create();
+        //     break;
+        //   case "schema:drop":
+        //     Schema.drop();
+        //     break;
+        //   case "schema:migrate":
+        //     Schema.migrate();
+        //     break;
+        //   case "schema:migrate:create":
+        //     Migration.create(splitInput[1], splitInput[2]);
+        //     break;
+        //   case "write":
+        //     System.out.print("DBLite> ");
+        //     // BufferedReader jsonInput = new BufferedReader(new InputStreamReader(System.in));
+        //     // Model.write(jsonInput);
+        //   case "exit":
+        //     return;
+        //   default:
+        //     System.out.println("Invalid input. Try again.");
+        // }
         call();
     } catch (IOException e) {
       System.out.println(e);
--- src/main/java/com/database/app/Writer.java
+++ src/main/java/com/database/app/Writer.java
@@ -1,6 +1,6 @@
 package com.database.app;
 
-import java.util.ArrayList;
+import java.util.Iterator;
 import java.io.File;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -9,12 +9,13 @@ import java.io.InputStream;
 import java.lang.StringBuilder;
 import java.io.FileReader;
 import java.io.InputStreamReader;
-
+import org.json.simple.JSONArray;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 
 public class Writer {
-  public static void write(String tableName) {
+  public static void write(String tableName, JSONObject inputObject) {
     try {
-    JSONObject inputObject = parseInput();
     JSONParser parser = new JSONParser();
     Object schema = parser.parse(new FileReader("schema.json"));
     JSONObject schemaObject = (JSONObject) schema;
@@ -22,10 +23,26 @@ public class Writer {
     JSONObject tableObject = (JSONObject) tablesObject.get(tableName);
     Table table = new Table(inputObject, tableObject);
     if (table.isValid()) {
-      System.out.println("Made it");
+      System.out.println("idFieldValue");
+
+      int idFieldValue = getLastIdFromTableName(tableName);
+      Iterator inputKeys = inputObject.keySet().iterator();
+      System.out.println("Writing id " + idFieldValue + " to " + tableName);
+      while(inputKeys.hasNext()) {
+        String inputKey = (String) inputKeys.next();
+        if (inputKey.equals("id")) {
+          writeToIdFile(tableName, Integer.toString(idFieldValue), inputObject);
+        } else {
+          System.out.println(inputObject.get(inputKey));
+          String inputKeyObject = (String) inputObject.get(inputKey).toString();
+          System.out.println(inputKey);
+          String fieldValue = inputKeyObject.toString();
+          writeToFiles(tableName, inputKey, fieldValue, idFieldValue);
+        }
+      }
     }
     } catch(Exception e) {
-
+      System.out.println(e);
     }
   }
   public static JSONObject parseInput() {
@@ -46,21 +63,69 @@ public class Writer {
     }
   }
 
-  public static void writeToFiles() {
-
+  public static void writeToIdFile(String tableName, String idFieldValue, JSONObject tableDataObject) {
+    String filepath = ".data/" + tableName + "/id/" + idFieldValue + ".json";
+    try {
+      File file = new File(filepath);
+      file.createNewFile();
+      BufferedWriter fileToWrite = new BufferedWriter(new FileWriter(file));
+      tableDataObject.put("id", idFieldValue);
+      fileToWrite.write(tableDataObject.toString());
+      fileToWrite.close();
+    } catch (Exception e) {
+      System.out.println(e);
+    }
   }
 
-  // public static String getLastIdFromTableName(String tableName) {
-  //   File migrationsDir = new File(".data/" + tableName + "/id/");
-  //   File[] files = migrationsDir.listFiles();
-  //   ArrayList<Integer> indices = new ArrayList<Integer>();
-  //   int maxId = 0;
-  //   for(File file : files) {
-  //     int id = String.parseInt(file.getName().split(".")[0]);
-  //     if (id > maxId) {
-  //       maxId = id;
-  //     }
-  //   }
-  //   return Integer.toString(currMax);
-  // }
+  public static void writeToFiles(String tableName, String fieldName, String fieldValue, int id) {
+    String filepath = ".data/" + tableName + "/" + fieldName + "/" + fieldValue + ".json";
+    try {
+      File file = new File(filepath);
+      System.out.println(file.exists());
+      if(file.exists()) {
+        JSONParser parser = new JSONParser();
+        JSONObject jsonObject = (JSONObject) parser.parse(new FileReader(filepath));
+        JSONArray ids = (JSONArray) (jsonObject.get("ids"));
+        ids.add(id);
+        JSONObject idsObject = new JSONObject();
+        idsObject.put("ids", ids);
+        BufferedWriter fileToWrite = new BufferedWriter(new FileWriter(filepath));
+        fileToWrite.write(idsObject.toJSONString());
+        fileToWrite.close();
+      } else {
+        File newFile = new File(filepath);
+        newFile.createNewFile();
+        JSONObject idsObject = new JSONObject();
+        JSONArray ids = new JSONArray();
+        ids.add(id);
+        idsObject.put("ids", ids);
+
+        BufferedWriter fileToWrite = new BufferedWriter(new FileWriter(newFile));
+        System.out.println(idsObject);
+
+        fileToWrite.write(idsObject.toJSONString());
+        fileToWrite.close();
+      }
+    } catch(Exception e) {
+      System.out.println(e);
+    }
+  }
+
+  public static int getLastIdFromTableName(String tableName) {
+    File migrationsDir = new File(".data/" + tableName + "/id/");
+    File[] files = migrationsDir.listFiles();
+    System.out.println(files);
+    int maxId = 0;
+    for(File file : files) {
+      System.out.println((file.getName()));
+      System.out.println(file.getName().toString().split(".json")[0]);
+      int id = Integer.parseInt(file.getName().toString().split(".json")[0]);
+
+
+      if (id > maxId) {
+        maxId = id;
+      }
+    }
+    return maxId + 1;
+  }
 }
